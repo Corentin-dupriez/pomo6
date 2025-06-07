24bde91b94114c8ecdb6cd779d7db0c3d6245dca
Corentin Dupriez <dupriez.c@icloud.com>
2025-06-07 16:05:36 +0300

    Added pagination. Worked on CSS of search and added view page

--- adverts/views.py
+++ adverts/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 from adverts.forms import AdvertForm
 from adverts.models import Advertisement
@@ -8,34 +9,44 @@ from adverts.models import Advertisement
 
 # Create your views here.
 def search_view(request):
-    query = request.GET.get('q')
+    query = request.GET.get('q', '').strip()
     category = request.GET.get('category')
     category_choices = Advertisement.CategoryChoices.choices
-    adverts = None
-    if query != '' and category is None:
-        adverts = Advertisement.objects.filter(Q(title__icontains=query) |
-                                               Q(description__icontains=query) |
-                                               Q(category__icontains=query))
-    if query != '' and query is not None and category is not None:
-        adverts = Advertisement.objects.filter((Q(title__icontains=query) |
+    adverts = Advertisement.objects.all()
+
+    if query:
+        adverts = adverts.filter(Q(title__icontains=query) |
+                                    Q(description__icontains=query) |
+                                    Q(category__icontains=query))
+
+    if category :
+        adverts = adverts.filter((Q(title__icontains=query) |
                                                Q(description__icontains=query) |
                                                Q(category__icontains=query)) &
                                                Q(category = category))
-    if (query == '' or query is None) and category is not None:
-        adverts = Advertisement.objects.filter(category=category)
-    if (query == '' or query is None) and category is None:
-        adverts = Advertisement.objects.all()
+
+
+    paginator = Paginator(adverts, 5)
+    page = request.GET.get('page')
+    page_obj = paginator.get_page(page)
 
     context = {
-        'adverts': adverts,
+        'adverts': page_obj,
+        'category': category,
+        'query': query,
         'category_choices': category_choices,
     }
 
     return render(request, 'search.html', context)
 
 def advert_view(request, pk: int, slug: str):
-    adverts = Advertisement.objects.filter(id=pk).first()
-    return HttpResponse(f'<h1>{adverts.title}</h1>')
+    advert = Advertisement.objects.filter(id=pk).first()
+
+    context = {
+        'advert' : advert
+    }
+
+    return render(request, 'view-listing.html', context)
 
 def create_ad_view(request):
     form = AdvertForm(request.POST or None)
@@ -45,3 +56,4 @@ def create_ad_view(request):
 
     context = {'form': form}
     return render(request, 'new_add.html', context)
+
