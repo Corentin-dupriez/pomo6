071bf1761f17baae75f609f6064054c4c8fdbd54
Corentin Dupriez <dupriez.c@icloud.com>
2025-08-01 01:18:17 +0300

    Added signal to index advert on creation

--- search_indexing/apps.py
+++ search_indexing/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class SearchIndexingConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'search_indexing'
+
+    def ready(self):
+        import search_indexing.signals
---
+++ search_indexing/signals.py
@@ -0,0 +1,11 @@
+from django.dispatch import receiver
+from adverts.models import Advertisement
+from pomo6 import settings
+from search_indexing.utils import index_ad
+from django.db.models import signals
+
+
+@receiver(signals.post_save, sender=Advertisement)
+def index_advertisement(sender, instance: Advertisement, created, **kwargs):
+    if created:
+        index_ad(instance)
